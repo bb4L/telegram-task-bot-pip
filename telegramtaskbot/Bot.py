5ebf84604d723d468f49feaecf6ec571049def41
bb4L <39266013+bb4L@users.noreply.github.com>
2020-11-02 21:27:20 +0100

    Version 0.1.2 (#4)
    * Add documentation
    
    * Fix minor bugs

--- telegramtaskbot/Bot.py
+++ telegramtaskbot/Bot.py
@@ -6,25 +6,60 @@ from typing import List
 import telegram
 from dotenv import load_dotenv
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, Filters, CallbackQueryHandler
+from telegram.ext import CallbackQueryHandler, CommandHandler, Filters, Updater
 
+from telegramtaskbot.Tasks.Task import Task
 from telegramtaskbot.Tasks.UrlTask import UrlTask
 
 
 class TelegramTaskBot(object):
+    """
+    A bot that can handle implementations of the different tasks.
+
+    Attributes
+    ----------
+    jobs : List[telegram.ext.Job]
+        list of the jobs registered
+
+    default_button_list : List[InlineKeyboardButton]
+        list of the default buttons
+
+    cmd_fun : dict
+        dictionary to map a command to a function
+
+    logger : Logger
+        the logger for the button
+
+    Methods
+    ----------
+
+    get_default_filter() -> telegram.ext.Filters.user
+        returns a user filter
+
+    start(self, update, context) -> None
+        gives the buttons to start or get tasks
+
+    run(self) -> None
+        Start the bot
+
+    handle_button(self, update, context) -> None
+        Function to handle button clicks
+    """
+
     jobs: List[telegram.ext.Job] = []
     default_button_list: List[InlineKeyboardButton] = []
     cmd_fun = {}
-    job_names = {}
     logger = logging.getLogger(__name__)
 
-    def __init__(self, tasks: []):
+    def __init__(self, tasks: List[Task]) -> None:
         load_dotenv()
         self.updater = Updater(token=os.getenv('BOT_TOKEN'), use_context=True)
         self.dispatcher = self.updater.dispatcher
         default_filter = self.get_default_filter()
-        self.dispatcher.add_handler(CommandHandler('start', self.start, default_filter))
-        self.dispatcher.add_handler(CallbackQueryHandler(self.handle_button, default_filter))
+        self.dispatcher.add_handler(CommandHandler(
+            'start', self.start, default_filter))
+        self.dispatcher.add_handler(CallbackQueryHandler(
+            self.handle_button, default_filter))
 
         self.TASKS = [task(self.updater.job_queue) for task in tasks]
 
@@ -38,13 +73,15 @@ class TelegramTaskBot(object):
                     self.dispatcher.add_handler(
                         CommandHandler(task.job_actual_value, task.get_actual_value_cmd, default_filter))
 
-                self.dispatcher.add_handler(CommandHandler(task.job_start_name, task.start_command, default_filter))
-                self.dispatcher.add_handler(CommandHandler(task.job_stop_name, task.stop_command, default_filter))
+                self.dispatcher.add_handler(CommandHandler(
+                    task.job_start_name, task.start_command, default_filter))
+                self.dispatcher.add_handler(CommandHandler(
+                    task.job_stop_name, task.stop_command, default_filter))
 
-        self.load_from_json()
+        self._load_from_json()
 
     @staticmethod
-    def get_default_filter():
+    def get_default_filter() -> telegram.ext.Filters.user:
         str_value = os.getenv('ALLOWED_USERS')
         if 'any' in str_value or 'ANY' in str_value:
             default_filter = None
@@ -53,44 +90,46 @@ class TelegramTaskBot(object):
             default_filter = Filters.user(user_id=allowed_users)
         return default_filter
 
-    def start(self, update, context):
-        reply_markup = InlineKeyboardMarkup(self.build_menu(self.default_button_list, n_cols=1))
+    def start(self, update, context) -> None:
+        reply_markup = InlineKeyboardMarkup(
+            self._build_menu(self.default_button_list, n_cols=1))
         context.bot.send_message(chat_id=update.effective_chat.id, text=os.getenv('START_MESSAGE'),
                                  reply_markup=reply_markup)
 
-    def run(self):
+    def run(self) -> None:
         self.updater.start_polling(clean=True)
 
-    def handle_button(self, update, context):
+    def handle_button(self, update, context) -> None:
         query = update.callback_query
         self.cmd_fun.get(query.data)(self.jobs, update, context)
-        self.save_to_json()
+        self._save_to_json()
         self.logger.info('after save')
 
-    def load_from_json(self):
+    def _load_from_json(self) -> None:
         try:
             with open('saved_jobs.json') as json_file:
                 data = json.load(json_file)
                 for job in data['jobs']:
                     for task in self.TASKS:
                         if task.job_name == job['name']:
-                            task._start(self.jobs, self.updater.job_queue, job['context'])
+                            task._start(
+                                self.jobs, self.updater.job_queue, job['context'])
                 self.logger.info(f'Loaded {len(data["jobs"])} from JSON')
         except IOError:
             self.logger.info("File not accessible")
 
-    def save_to_json(self):
+    def _save_to_json(self) -> None:
         data = {'jobs': []}
         for job in self.jobs:
             data['jobs'].append({
                 'context': job.context,
                 'name': job.name,
             })
-        with open('saved_jobs.json', 'w') as outfile:
+        with open('saved_jobs.json', 'w+') as outfile:
             json.dump(data, outfile)
 
     @staticmethod
-    def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+    def _build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None) -> List[InlineKeyboardButton]:
         menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
         if header_buttons:
             menu.insert(0, header_buttons)
--- telegramtaskbot/Tasks/UrlTask.py
+++ telegramtaskbot/Tasks/UrlTask.py
@@ -1,21 +1,62 @@
 import time as timer
 from abc import abstractmethod
+from typing import List
 
 import requests
 import telegram
-from telegram.ext import JobQueue
-
 from requests import Response
 from telegram import InlineKeyboardButton
-
+from telegram.ext import JobQueue
 from telegramtaskbot.Tasks.GenericTask import GenericTask
 
 
 class UrlTask(GenericTask):
+    """
+    A task based on a http request.
+
+    Attributes
+    ----------
+    job_name : str
+        the name of the task
+
+    disable_notifications : bool
+        bool if notifications should be disabled or not
+
+    url : str
+        the url where the data for the message lies
+
+    Methods
+    ----------
+    callback(self, context: telegram.ext.CallbackContext) -> None
+        Execute the task and use the context to send the message.
+
+    get_actual_value(self, joblist: [], update: telegram.Update, context: telegram.ext.CallbackContext) -> None
+        Returns the current value for the task.
+
+    get_actual_value_cmd(self, update: telegram.Update, context: telegram.ext.CallbackContext) -> None
+        Called whe tapping on the button to get the actual value
+
+    handle_get_actual_value(self, context: telegram.ext.CallbackContext, chat_id: str) -> None
+        Writes te actual walue to the given context and chat_id
+
+    get_data(self) -> str
+        Returns the data/message text to be send
+
+    handle_response(self, response: Response) -> str
+        Handles the response and transforms it into the message text to be send
+
+    get_response(self) -> Response
+        Returns the response for the given url
+
+    get_inline_keyboard(self) -> List[InlineKeyboardButton]
+        Returns the list of buttons to be shown
+    """
+
+    job_name: str
     disable_notifications = True
     url: str
 
-    def callback(self, context: telegram.ext.CallbackContext):
+    def callback(self, context: telegram.ext.CallbackContext) -> None:
         self.logger.info(f'Run {self.job_name}')
         users = self.load_users()
         response_message = self.get_data()
@@ -29,26 +70,41 @@ class UrlTask(GenericTask):
                 self.logger.info(f'Error occurred while notifying {user}')
                 self.logger.error(e.message)
 
-    def get_actual_value(self, joblist: [], update: telegram.Update, context: telegram.ext.CallbackContext):
-        self.handle_get_actual_value(context, update.callback_query.message.chat_id)
+    def get_actual_value(self, joblist: [], update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+        """Function for te mapping to send the actual value after button is pressed."""
 
-    def get_actual_value_cmd(self, update: telegram.Update, context: telegram.ext.CallbackContext):
+        self.handle_get_actual_value(
+            context, update.callback_query.message.chat_id)
+
+    def get_actual_value_cmd(self, update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+        """Handles command to get the actual value."""
+
         self.handle_get_actual_value(context, update.message.chat_id)
 
-    def handle_get_actual_value(self, context: telegram.ext.CallbackContext, chat_id: str):
-        self.logger.debug(f'Get actual value from {self.job_name} for {chat_id}')
-        data: str = self.get_data()
-        self.logger.debug(f'Send message to {chat_id} with content: \"{" ".join(data.splitlines())}\"')
+    def handle_get_actual_value(self, context: telegram.ext.CallbackContext, chat_id: str) -> None:
+        """Send the actual value/message with the given context to the chat_id."""
+
+        self.logger.debug(
+            f'Get actual value from {self.job_name} for {chat_id}')
+        data = self.get_data()
+        self.logger.debug(
+            f'Send message to {chat_id} with content: \"{" ".join(data.splitlines())}\"')
         context.bot.send_message(chat_id=chat_id, text=data)
 
-    def get_data(self):
+    def get_data(self) -> str:
+        """Returns the data to be send as message."""
+
         return self.handle_response(self.get_response())
 
     @abstractmethod
-    def handle_response(self, response: Response):
+    def handle_response(self, response: Response) -> str:
+        """Handle the given response and return the actual message to be send."""
+
         pass
 
-    def get_response(self):
+    def get_response(self) -> Response:
+        """Returns the response for the set url."""
+
         count = 0
         response = requests.get(self.url)
         if response.status_code != 200:
@@ -60,11 +116,16 @@ class UrlTask(GenericTask):
         self.logger.debug(f'{self.job_name} tried for {count} times')
         return response
 
-    def get_inline_keyboard(self):
+    def get_inline_keyboard(self) -> List[InlineKeyboardButton]:
+        """Returns a List[InlineKeyboardButton] containing all the relevant buttons."""
+
         buttons = [
-            InlineKeyboardButton(f"Get actual Value for {self.job_name}", callback_data=self.job_actual_value),
+            InlineKeyboardButton(
+                f"Get actual Value for {self.job_name}", callback_data=self.job_actual_value),
         ]
         if self.show_subscribe_buttons:
-            buttons.append(InlineKeyboardButton(f"Subscribe for {self.job_name}", callback_data=self.job_start_name))
-            buttons.append(InlineKeyboardButton(f"Unsubscribe for {self.job_name}", callback_data=self.job_stop_name))
+            buttons.append(InlineKeyboardButton(
+                f"Subscribe for {self.job_name}", callback_data=self.job_start_name))
+            buttons.append(InlineKeyboardButton(
+                f"Unsubscribe for {self.job_name}", callback_data=self.job_stop_name))
         return buttons
